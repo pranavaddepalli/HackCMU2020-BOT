b3b892dae23cef13f143ca919a8b87e899a8360a
Jai Kumar <jkumar0212@gmail.com>
2020-09-26 11:36:46 -0400

    actually got the bot to suppress its message this time

--- bot.py
+++ bot.py
@@ -16,7 +16,8 @@ async def on_message(message):
         vidURL = message.content[7:]
         vidID = vidURL[vidURL.index('=')+1:]
         watchLink = f'https://youtube-party-bot.herokuapp.com/api/{vidID}'
-        await watchLink.edit(suppress=True)
-        await message.channel.send('Happy watching! ' +  watchLink)
+        botmessage = await message.channel.send('Happy watching! ' +  watchLink)
+        await botmessage.edit(suppress=True)
 
-client.run(os.environ['BOT_TOKEN'])
+#client.run(os.environ['BOT_TOKEN'])
+client.run('NzU5MTkyMzcwOTUxNzQ5NjUy.X256_g.z-tmYdLSEFQ8hgeBTGpQw4N8WJw')
